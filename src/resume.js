449b5296145a3d80b4271fab9249e6109f46ebeb
Liran Bruchim <liran.bruchim@gmail.com>
2024-01-30 11:45:20 +0200

    with git actions

--- src/resume.js
+++ src/resume.js
@@ -12,8 +12,11 @@ export default class resume extends Component {
             <div className='resumePage'>
                 <h1 >Liran Bruchim</h1>
                 <p>Welcome to my app store</p>
+                <p>For this page I used Google Cloud Project infrastructure with Git Actions for the deployment</p>
                 <p>In this app you can add items to your admin page and sell it in the home page</p>
-                <p>please continue by pressing the "Admin store" button</p>
+                
+                <p>please continue by pressing the "Admin store" button </p>
+                
                 <button onClick={this.adminPage} >Admin Store</button>
             </div>
         )
