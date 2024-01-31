9161f6bbb7f17da41cb35273dac7b06d452afc3f
Liran Bruchim <liran.bruchim@gmail.com>
2024-01-31 12:17:58 +0200

    change buttons

--- src/resume.js
+++ src/resume.js
@@ -11,13 +11,22 @@ export default class resume extends Component {
         return (
             <div className='resumePage'>
                 <h1 >Liran Bruchim</h1>
+                <a href="https://www.linkedin.com/in/liran-bruchim-045ba7135/" target="_blank" class="btn-linkedin" >linkedin</a>
                 <p>Welcome to my app store</p>
-                <p>For this page I used Google Cloud Project infrastructure with Git Actions for the deployment - 2</p>
+                <p>For this app I used Google Cloud Project infrastructure with Git Actions for the deployment - 2</p>
                 <p>In this app you can add items to your admin page and sell it in the home page</p>
                 
                 <p>please continue by pressing the "Admin store" button </p>
                 
-                <button onClick={this.adminPage} >Admin Store</button>
+                <button onClick={this.adminPage} class="button-82-pushable" role="button">
+                <span class="button-82-shadow"></span>
+                <span class="button-82-edge"></span>
+                <span class="button-82-front text">
+                
+                Admin Store
+                </span>
+                </button>
+
             </div>
         )
     }
